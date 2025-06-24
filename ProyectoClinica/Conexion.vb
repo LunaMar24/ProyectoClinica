e9e4472c15d85db2b5c45e96d7c386c7f490efa2
Marcela Jimenez <marcelamja@gmail.com>
2025-06-23 22:41:57 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoClinica/Conexion.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Conexion
+    Private conexion As MySqlConnection
+
+    Public Sub New()
+        Dim cadena As String = "server=localhost;user=root;password=1234;database=login;"
+        conexion = New MySqlConnection(cadena)
+    End Sub
+
+    Public Function Open() As MySqlConnection
+        If conexion.State = ConnectionState.Closed Then
+            conexion.Open()
+            'MessageBox.Show("Hola la conexion es exitosa")
+
+        End If
+        Return conexion
+    End Function
+
+    Public Sub Cerrar()
+        If conexion.State = ConnectionState.Open Then
+            conexion.Close()
+            'MessageBox.Show("Hola la conexion se cerró correctamente")
+        End If
+    End Sub
+End Class
---
+++ ProyectoClinica/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()>
+  Private Sub InitializeComponent()
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+    Panel1 = New Panel()
+    txtPassword = New TextBox()
+    txtUsuario = New TextBox()
+    btnSalir = New Button()
+    btnIngresar = New Button()
+    lblPassword = New Label()
+    lblUsuario = New Label()
+    lblBienvenidos = New Label()
+    Panel1.SuspendLayout()
+    SuspendLayout()
+    ' 
+    ' Panel1
+    ' 
+    Panel1.BackColor = Color.FromArgb(CByte(187), CByte(212), CByte(235))
+    Panel1.BorderStyle = BorderStyle.Fixed3D
+    Panel1.Controls.Add(txtPassword)
+    Panel1.Controls.Add(txtUsuario)
+    Panel1.Controls.Add(btnSalir)
+    Panel1.Controls.Add(btnIngresar)
+    Panel1.Controls.Add(lblPassword)
+    Panel1.Controls.Add(lblUsuario)
+    Panel1.Location = New Point(46, 60)
+    Panel1.Name = "Panel1"
+    Panel1.Size = New Size(222, 265)
+    Panel1.TabIndex = 0
+    ' 
+    ' txtPassword
+    ' 
+    txtPassword.Location = New Point(68, 143)
+    txtPassword.Name = "txtPassword"
+    txtPassword.PasswordChar = "*"c
+    txtPassword.Size = New Size(127, 23)
+    txtPassword.TabIndex = 5
+    ' 
+    ' txtUsuario
+    ' 
+    txtUsuario.Location = New Point(68, 52)
+    txtUsuario.Name = "txtUsuario"
+    txtUsuario.Size = New Size(127, 23)
+    txtUsuario.TabIndex = 4
+    ' 
+    ' btnSalir
+    ' 
+    btnSalir.Location = New Point(120, 214)
+    btnSalir.Name = "btnSalir"
+    btnSalir.Size = New Size(75, 23)
+    btnSalir.TabIndex = 3
+    btnSalir.Text = "Salir"
+    btnSalir.UseVisualStyleBackColor = True
+    ' 
+    ' btnIngresar
+    ' 
+    btnIngresar.Location = New Point(16, 214)
+    btnIngresar.Name = "btnIngresar"
+    btnIngresar.Size = New Size(75, 23)
+    btnIngresar.TabIndex = 2
+    btnIngresar.Text = "Ingresar"
+    btnIngresar.UseVisualStyleBackColor = True
+    ' 
+    ' lblPassword
+    ' 
+    lblPassword.AutoSize = True
+    lblPassword.Location = New Point(26, 105)
+    lblPassword.Name = "lblPassword"
+    lblPassword.Size = New Size(63, 15)
+    lblPassword.TabIndex = 1
+    lblPassword.Text = "Password :"
+    ' 
+    ' lblUsuario
+    ' 
+    lblUsuario.AutoSize = True
+    lblUsuario.Location = New Point(26, 21)
+    lblUsuario.Name = "lblUsuario"
+    lblUsuario.Size = New Size(53, 15)
+    lblUsuario.TabIndex = 0
+    lblUsuario.Text = "Usuario :"
+    ' 
+    ' lblBienvenidos
+    ' 
+    lblBienvenidos.AutoSize = True
+    lblBienvenidos.BackColor = Color.FromArgb(CByte(208), CByte(226), CByte(243))
+    lblBienvenidos.Location = New Point(41, 13)
+    lblBienvenidos.Name = "lblBienvenidos"
+    lblBienvenidos.Size = New Size(166, 15)
+    lblBienvenidos.TabIndex = 1
+    lblBienvenidos.Text = "Bienvenidos a Clinica Server...."
+    ' 
+    ' frmLogin
+    ' 
+    AutoScaleDimensions = New SizeF(7F, 15F)
+    AutoScaleMode = AutoScaleMode.Font
+    BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+    BackgroundImageLayout = ImageLayout.Stretch
+    ClientSize = New Size(642, 371)
+    Controls.Add(lblBienvenidos)
+    Controls.Add(Panel1)
+    DoubleBuffered = True
+    FormBorderStyle = FormBorderStyle.FixedToolWindow
+    MaximumSize = New Size(658, 410)
+    MinimizeBox = False
+    MinimumSize = New Size(658, 410)
+    Name = "frmLogin"
+    Text = "Login"
+    Panel1.ResumeLayout(False)
+    Panel1.PerformLayout()
+    ResumeLayout(False)
+    PerformLayout()
+  End Sub
+
+  Friend WithEvents Panel1 As Panel
+  Friend WithEvents btnSalir As Button
+  Friend WithEvents btnIngresar As Button
+  Friend WithEvents lblPassword As Label
+  Friend WithEvents lblUsuario As Label
+  Friend WithEvents txtPassword As TextBox
+  Friend WithEvents txtUsuario As TextBox
+  Friend WithEvents lblBienvenidos As Label
+
+End Class
