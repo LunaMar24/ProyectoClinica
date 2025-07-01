7618b410d74d929e6e7a4620fa47476c221a50ae
Marcela Jimenez <marcelamja@gmail.com>
2025-06-30 18:31:32 -0600

    correccion de error

--- ProyectoClinica/Conexion.vb
+++ ProyectoClinica/Conexion.vb
@@ -1,11 +1,11 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Conexion
-    Private conexion As MySqlConnection
+  Private conexion As MySqlConnection
 
-    Public Sub New()
+  Public Sub New()
     Dim cadena As String = "server=localhost;user=root;password=1234;database=db_clinica;"
     conexion = New MySqlConnection(cadena)
-    End Sub
+  End Sub
 
   Public Function Abrir() As MySqlConnection
     If conexion.State = ConnectionState.Closed Then
@@ -17,9 +17,9 @@ Public Class Conexion
   End Function
 
   Public Sub Cerrar()
-        If conexion.State = ConnectionState.Open Then
-            conexion.Close()
-            'MessageBox.Show("Hola la conexion se cerró correctamente")
-        End If
-    End Sub
+    If conexion.State = ConnectionState.Open Then
+      conexion.Close()
+      'MessageBox.Show("Hola la conexion se cerró correctamente")
+    End If
+  End Sub
 End Class
